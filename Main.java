7da58a3a16d603f4d9069af88f98c7f7420888f1
aj <ali.becerra1224@gmail.com>
2021-02-28 21:34:48 -0800

    adding main myframe

--- Main.java
+++ Main.java
@@ -1,5 +1,16 @@
-class Main {
-  public static void main(String args[]) {
-    System.out.println("Hello, world!");
+import javax.swing.JFrame;
+import javax.swing.ImageIcon;
+import java.awt.Color;
+import javax.swing.JLabel;
+import java.util.Scanner;
+
+public class Main{
+
+  public static void main(String [] args){
+
+    MyFrame frame1 = new MyFrame();
+    //System.out.println("what the");
+    frame1.MyFrame();
+
   }
 }
