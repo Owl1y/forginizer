d6a53f7b256a0b1890ce9ff8f2264ae0b44dade4
ali <ali.becerra1224@gmail.com>
2021-03-23 00:52:32 -0700

    adding the code from the github to this branch

--- Main.java
+++ Main.java
@@ -9,7 +9,6 @@ public class Main{
   public static void main(String [] args){
 
     MyFrame frame1 = new MyFrame();
-    //System.out.println("what the");
     frame1.MyFrame();
 
   }
--- MyFrame.java
+++ MyFrame.java
@@ -1,20 +1,47 @@
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
 import java.awt.Color;
-
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import java.awt.Image;
 
 public class MyFrame extends JFrame{
 
   public void MyFrame(){
 
+   JLabel title = new JLabel();                       //this intilizes the text
+   ImageIcon Fileimg = new ImageIcon("filePhoto.png");//putting a photo onto the GUI
+   title.setText("File organizer");                   //trying to put text on the GUI
+
+   Image smallerFileImg = Fileimg.getImage();                                                       //getting the folder image and turning it into an image to scale it down
+   Image smallingFileimg = smallerFileImg.getScaledInstance(250, 250, java.awt.Image.SCALE_SMOOTH); //this is what scales the image down (x, y, method to scale it down)
+   Fileimg = new ImageIcon(smallingFileimg);                                                        //putting the smaller image into the main file image
+
+
    MyFrame Frame = new MyFrame();
 
-   ImageIcon image = new ImageIcon("genealogy.png");
-   Frame.setTitle("Verminator");
-   Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   Frame.setSize(800, 920);
-   Frame.setVisible(true);
-   Frame.setIconImage(image.getImage());
-   Frame.getContentPane().setBackground(new Color(150, 200, 181));
+   Frame.setLayout(null);
+   JPanel shrinkPhoto = new JPanel();                  //These 3 lines of code make the new panel
+   shrinkPhoto.setBackground(new Color(150, 200, 181));//color of the
+   shrinkPhoto.setBounds(0, 0, 250, 250);              //puts the panel on the upper left of the GUI with a size of 250x250
+
+   JLabel folderPNG = new JLabel();
+   folderPNG.setIcon(Fileimg);
+
+   ImageIcon geneTree = new ImageIcon("genealogy.png");//photo for the thing in the corner
+   Frame.setTitle("Verminator");// just the place holder name cause I thought it was funny
+   Frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//This makes sure the when you click the X on the GUI it closes
+   Frame.setSize(800, 920);//the resolution of the GUI
+   Frame.setVisible(true);//This just makes sure the GUI is visible
+   Frame.setIconImage(geneTree.getImage());//this is what puts the photo in the corner of the GUI
+   Frame.getContentPane().setBackground(new Color(150, 200, 181));//150, 200, 181[also the color of the GUI]
+   Frame.add(title);//trying to add text onto the GUI
+
+   Frame.add(shrinkPhoto);// adds the panel to the GUI to put the file folder.png onto the panel to make it smaller
+   shrinkPhoto.add(folderPNG);// the photo folder.png is added on the panel as if the panel is its own GUI
+
+
+
  }
 }
